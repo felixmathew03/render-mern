4556c8c1c8c0d5854af23f8a0198fcddeae5d25b
felixmathew03 <felixmathewtt@gmail.com>
2026-02-04 10:29:04 +0530

    small updation in appjs

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,6 @@ import env from 'dotenv';
 
 const app = express();
 env.config();
-app.use(cors({
-  origin:'http://localhost:5173'
-}));
 app.use(express.json());
 
 // MongoDB connection
@@ -23,13 +20,11 @@ app.use('/api/todos',routes)
 const __dirname = path.resolve();
 
 // Serve frontend
-if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*splat", (req, res) =>
     res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
   );
-}
 
 const PORT =  process.env.PORT || 5000;
 app.listen(PORT, () => {
